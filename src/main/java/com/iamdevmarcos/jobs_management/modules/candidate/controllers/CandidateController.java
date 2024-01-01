28a547133e51f594c24c7181839e78c7960dab24
Marcos Mendes <marcosdev.me@gmail.com>
2024-01-01 12:57:09 -0300

    Add Candidate Validation

--- src/main/java/com/iamdevmarcos/jobs_management/modules/candidate/controllers/CandidateController.java
+++ src/main/java/com/iamdevmarcos/jobs_management/modules/candidate/controllers/CandidateController.java
@@ -1,6 +1,7 @@
 package com.iamdevmarcos.jobs_management.modules.candidate.controllers;
 
 import com.iamdevmarcos.jobs_management.modules.candidate.entities.Candidate;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/candidate")
 public class CandidateController {
     @PostMapping
-    public void createCandidate(@RequestBody Candidate candidate) {
+    public void createCandidate(@Valid @RequestBody Candidate candidate) {
         System.out.println(candidate.getUsername());
     }
 }
--- src/main/java/com/iamdevmarcos/jobs_management/modules/candidate/entities/Candidate.java
+++ src/main/java/com/iamdevmarcos/jobs_management/modules/candidate/entities/Candidate.java
@@ -1,7 +1,10 @@
 package com.iamdevmarcos.jobs_management.modules.candidate.entities;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 import java.util.UUID;
 
@@ -9,9 +12,18 @@ import java.util.UUID;
 public class Candidate {
     private UUID id;
     private String name;
+
+    @Pattern(regexp = "^(?!\\s*$).+", message = "O campo [username] não pode conter espaços")
     private String username;
+
+    @Email(message = "O campo [email] deve conter um e-mail válido!")
     private String email;
+
+    @Length(min = 10, max = 100)
     private String password;
+
+    @Length(max = 255)
     private String description;
+
     private String curriculum;
 }
