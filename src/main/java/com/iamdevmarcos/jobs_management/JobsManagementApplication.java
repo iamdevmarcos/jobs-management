6e2035c3bd1a94e5a1ebf77d3ebe60e5abd967df
Marcos Mendes <marcosdev.me@gmail.com>
2024-01-01 12:41:48 -0300

    Initial Commit

---
+++ src/main/java/com/iamdevmarcos/jobs_management/JobsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.iamdevmarcos.jobs_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobsManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iamdevmarcos/jobs_management/JobsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iamdevmarcos.jobs_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobsManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
