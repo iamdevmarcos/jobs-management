1e49f316ec9f592dc2ffe1b51c0cab1efe75e5da
Marcos Mendes <marcosdev.me@gmail.com>
2024-01-22 14:35:43 -0300

    Encoding Company Password

--- src/main/java/com/iamdevmarcos/jobs_management/modules/company/useCases/CreateCompanyUseCase.java
+++ src/main/java/com/iamdevmarcos/jobs_management/modules/company/useCases/CreateCompanyUseCase.java
@@ -4,6 +4,7 @@ import com.iamdevmarcos.jobs_management.exceptions.UserFoundException;
 import com.iamdevmarcos.jobs_management.modules.company.entities.CompanyEntity;
 import com.iamdevmarcos.jobs_management.modules.company.repositories.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,12 +12,18 @@ public class CreateCompanyUseCase {
     @Autowired
     private CompanyRepository companyRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CompanyEntity execute(CompanyEntity companyEntity) {
         this.companyRepository
             .findByCompanyUsernameOrCompanyEmail(companyEntity.getCompanyUsername(), companyEntity.getCompanyEmail())
             .ifPresent(user -> {
                 throw new UserFoundException();
             });
+
+        var encodedPassword = passwordEncoder.encode(companyEntity.getCompanyPassword());
+        companyEntity.setCompanyPassword((encodedPassword));
 
         return this.companyRepository.save(companyEntity);
     }
