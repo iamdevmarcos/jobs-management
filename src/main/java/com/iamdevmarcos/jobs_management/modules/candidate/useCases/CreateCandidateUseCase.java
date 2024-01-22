0982ebd2046d6c326a2059f2896a56ee0aa71caf
Marcos Mendes <marcosdev.me@gmail.com>
2024-01-22 14:35:56 -0300

    Encoding Candidate Password

--- src/main/java/com/iamdevmarcos/jobs_management/modules/candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/com/iamdevmarcos/jobs_management/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -4,6 +4,7 @@ import com.iamdevmarcos.jobs_management.exceptions.UserFoundException;
 import com.iamdevmarcos.jobs_management.modules.candidate.entities.CandidateEntity;
 import com.iamdevmarcos.jobs_management.modules.candidate.repositories.CandidateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,12 +12,18 @@ public class CreateCandidateUseCase {
     @Autowired
     private CandidateRepository candidateRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CandidateEntity execute(CandidateEntity candidate) {
         this.candidateRepository
                 .findByUsernameOrEmail(candidate.getUsername(), candidate.getEmail())
                 .ifPresent(user -> {
                     throw new UserFoundException();
                 });
+
+        var encodedPassoword = passwordEncoder.encode(candidate.getPassword());
+        candidate.setPassword(encodedPassoword);
 
         return this.candidateRepository.save(candidate);
     }
